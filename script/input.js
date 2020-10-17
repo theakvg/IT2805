e58ea6258f67f481436600950ac24349faf53253
na2lie <natsorfor@gmail.com>
2020-10-17 18:35:22 +0200

    CSS på bestilling.html

---
+++ script/input.js
@@ -0,0 +1,17 @@
+function datoFocus() {
+    var dato = document.getElementById("dato");
+    if (dato === document.activeElement) {
+        dato.type="date";
+    } else {
+        dato.type="text";
+    }
+}
+
+function tidspunktFocus() {
+    var tidspunkt = document.getElementById("tidspunkt");
+    if (tidspunkt === document.activeElement) {
+        tidspunkt.type="time";
+    } else {
+        tidspunkt.type="text";
+    }
+}
