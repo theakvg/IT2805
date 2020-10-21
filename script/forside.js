250aa7a7dab46fed721374ba875368193d5e0817
theakvg <thea.kvinnegard@gmail.com>
2020-10-21 11:13:47 +0200

    laget bildekarusellen ferdig, trenger css

--- script/forside.js
+++ script/forside.js
@@ -4,32 +4,40 @@ let imgBK = document.getElementById("imgBK");
 const classBytt = document.getElementsByClassName("classBytt");
 
 
-classBytt[0].addEventListener("click", mainByttBilde, true);
-classBytt[1].addEventListener("click", mainByttBilde, true); 
+let bildeListe = ["images/forsidemat.jpg", "images/forsidemat2.jpg", "images/logo.png", "images/nyhet1.jpg", "images/nyhet2.jpg"];
 
-let bildeListe = ["forsidemat.jpg", "forsidemat2.jpg", "logo.png", "nyhet1.png", "nyhet2.png"];
+classBytt[0].addEventListener("click", tilVenstre, true);
+classBytt[1].addEventListener("click", tilHoyre, true); 
 
-tall = 0; 
 
-function nesteBilde(i) {
-    imgBK.src = bildeListe[i];
-};
-
-function tilVenstre() {
-     tall = tall -1; 
-     nesteBilde(tall);
+function tilHoyre() {
+    let imgSrc = imgBK.getAttribute("src");
+    for (i = 0; i < bildeListe.length; i++){
+        if (bildeListe[i] === imgSrc){
+            let a = 0;
+            if (i > bildeListe.length-2) {
+                a = 0; 
+                console.log(a + "0");
+            } else {
+                a = i + 1;
+            };
+            imgBK.src = bildeListe[a];
+        };
+    };
 }; 
- function tilHøyre() {
-     tall += 1; 
-     nesteBilde(tall); 
- }; 
+ function tilVenstre() {
+    let imgSrc = imgBK.getAttribute("src");
+    for (i = 0; i < bildeListe.length; i++){
+        if (bildeListe[i] === imgSrc){
+            let a = 0;
+            if (i === 0) {
+                a = bildeListe.length - 1; 
+            } else {
+                a = i - 1;
+            };
+            imgBK.src = bildeListe[a];
+        };
+    };
+}; 
 
- function mainByttBilde() {
-     console.log("for funka");
-     if (btnVenstre.onclick === false) {
-         console.log("venstre klikk");
-     } else if (btnHoyre.onclick === true) {
-         console.log("høyre klikk");
-     };
- };
 
