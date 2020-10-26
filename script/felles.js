da9eceba20d2506b07591e3e5d3363e7ee9c82f5
Synne <synne.odegaard@gmail.com>
2020-10-26 14:15:49 +0100

    Endret litt css

--- script/felles.js
+++ script/felles.js
@@ -1,10 +1,9 @@
-// const headerAll = '<a id="logoLink" href="forside.html"><img id="logo" src="images/logo.png" alt="Logo"></a><h1 id="overskrift">Stemning</h1><a href="info.html">OM OSS</a><a href="bestilling.html">BESTILL BORD</a><a href="meny.html">MENY</a><a href="forside.html">FORSIDE</a>';
+//const headerAll = '<a id="logoLink" href="forside.html"><img id="logo" src="images/logo.png" alt="Logo"></a><h1 id="overskrift">Stemning</h1><a href="info.html">OM OSS</a><a href="bestilling.html">BESTILL BORD</a><a href="meny.html">MENY</a><a href="forside.html">FORSIDE</a>';
 // const footerAll = '<div><p><strong>Kontakt oss:</strong><br>431 56 761 <br>eliseraade@stemning.no</p></div><div><p><strong>Adresse:</strong><br>Noeveien 4b <br>4113 Drøbak</p></div><div><p><strong>Åpningstider:</strong><br>MAN-FRE: 10.00-19.00 <br>LØR: 10.00-18.00 <br>SØN: 12.00-18.00</p></div><div><p><strong>Sosiale medier:</strong></p><a href=""><img src="images/instagram.png" alt="Instagram"></a><a href="Facebook"><img src="images/facebook.png" alt=""></a></div>';
  //  All html'en til header og footer elementene ligger inne i disse const'ene. Dette er fordi det ikke er mulig å dele html kode mellom flere sider utenom på denne måten. 
  //  Hvis denne skal forandres på, anbefales det å lime den inn i et html dokument, gjøre forandringene, og lime det inn igjen. 
-
+ 
 const sider = ["meny", "felles", "forside", "info", "bestilling"]; // liste med alle sidene, for alle header eller footer elemente skal hete header/footer + sidenavn. 
-
 
 function includeHTML() {        // lager funksjonen
     for (i = 0; i < sider.length; i++) {    // sjekker antallet i listen "sider" og kjører gjennom hvert element. 
@@ -18,6 +17,7 @@ function includeHTML() {        // lager funksjonen
 };
 
 // includeHTML();      // kjører funksjonen
+
 
 function showBurgerMeny() {
     let burgerMeny = document.getElementById("burgerMeny");
