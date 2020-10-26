d61abaa4764e2998581cb216f30786c872a00d02
Synne <synne.odegaard@gmail.com>
2020-10-26 15:18:18 +0100

    Kommentarer

--- script/forside.js
+++ script/forside.js
@@ -4,11 +4,26 @@ const btnHoyre = document.getElementById("btnHoyre");
 let imgBK = document.getElementById("imgBK");
 const classBytt = document.getElementsByClassName("classBytt");
 
-//Setter bildene som skal brukes i bildekarusellen i en liste
+//Settersrc til bildene som skal brukes i bildekarusellen i en liste
 let bildeListe = ["images/Bildekarusell/forsidemat.jpg", "images/Bildekarusell/forsidemat2.jpg", "images/Bildekarusell/forsidemat3.jpg", "images/Bildekarusell/forsidemat4.jpg", "images/Bildekarusell/forsidemat5.jpg", "images/Bildekarusell/forsidemat6.jpg"];
 
 classBytt[0].addEventListener("click", tilVenstre); //Kjører funksjonen tilVenstre når knappen til venstre i karusellen klikkes på
 classBytt[1].addEventListener("click", tilHoyre); //Kjører funksjonen tilHoyre når knappen til høyre i karusellen klikkes på
+
+function tilVenstre() {
+    let imgSrc = imgBK.getAttribute("src"); //Lager en variabel imgSrc som er lik src til bildet imgBK
+    for (i = 0; i < bildeListe.length; i++){
+        if (bildeListe[i] === imgSrc){
+            let a = 0;
+            if (i === 0) {
+                a = bildeListe.length - 1; 
+            } else {
+                a = i - 1;
+            };
+            imgBK.src = bildeListe[a];
+        };
+    };
+}; 
 
 function tilHoyre() {
     let imgSrc = imgBK.getAttribute("src");
@@ -26,19 +41,5 @@ function tilHoyre() {
     };
 }; 
 
- function tilVenstre() {
-    let imgSrc = imgBK.getAttribute("src");
-    for (i = 0; i < bildeListe.length; i++){
-        if (bildeListe[i] === imgSrc){
-            let a = 0;
-            if (i === 0) {
-                a = bildeListe.length - 1; 
-            } else {
-                a = i - 1;
-            };
-            imgBK.src = bildeListe[a];
-        };
-    };
-}; 
 
 
