e0b88c38d6d517fad2b6bf8f0b6cc106c5b021d2
theakvg <thea.kvinnegard@gmail.com>
2020-10-26 14:22:15 +0100

    jobbet med input

--- script/input.js
+++ script/input.js
@@ -32,6 +32,7 @@ function dagensDato() {
     date = n.getDate();
     dato.min = year + "-" + minMonth + "-" + date;
     maxMonth = n.getMonth() + 4;
+    
     if (maxMonth > 12) {
         maxMonth = maxMonth - 12; 
         year += 1; 
@@ -42,12 +43,12 @@ function dagensDato() {
     dato.max = year + "-" + maxMonth + "-" + date;
 };
 
-function riktigTid() {
-    let tid = document.getElementById("tidspunkt");
-    if (tid.validity.valid === false) {
-        alert("Våre åpningstider er mellom 11.00 og 20.00, vennligst velg et tidspunkt mellom disse tidspunktene.");
-    };
-};
+//function riktigTid() {
+//    let tid = document.getElementById("tidspunkt");
+//    if (tid.validity.valid === false) {
+//        alert("Våre åpningstider er mellom 11.00 og 20.00, vennligst velg et tidspunkt mellom disse tidspunktene.");
+//    };
+//};
 
 const listeSendInn = [["navn", "Navn", "Fyll inn navnet ditt"], ["mobil", "Mobil", "Fyll inn mobilnummeret"], ["email", "Email", "Fyll inn emailen din med riktig format: example@example.com"], ["antallBesokende", "Antall besøkende", "Antall besøkende kan ikke overstige 10. Hvis dere er flere enn 10, vennligst ring kafeen for å booke."], ["dato", "Dato", "Du kan bare booke bord fra dagens dato og tre måneder fram i tid"], ["tidspunkt", "Tidspunkt", "Våre åpningstider er mellom 11.00 og 20.00, vennligst bestill bord mellom disse tidspunktene"], ["kommentarer", "Kommentarer"]];
  
@@ -58,14 +59,41 @@ function alertInput(event) {
     event.preventDefault();
     console.log("??");
     let skrivUt = ""; 
+    let feilMelding = "";
+    let oversikt = 0;
     for (let i = 0; i < listeSendInn.length; i++) {
-        let element = document.getElementById(listeSendInn[i][0]).value;
-        console.log(element);
-        skrivUt = skrivUt + "\n" + listeSendInn[i][1] + ": " + element; 
+        if (listeSendInn[i][0] === "tidspunkt") {
+            
+            let tid = document.getElementById("tidspunkt");
+            let n = new Date();
+            let time = n.getHours();
+            let min = ("0" + (n.getMinutes())).slice(-2);
+            let a = tid.value;
+            
+            if (a.slice(0, 2) < time) {
+                console.log("what");
+                if (a.slice(3, 5) < min) {
+                    tidspunkt.validity.valid = false; 
+                };
+            };
+        };
+        let element = document.getElementById(listeSendInn[i][0]);
+        skrivUt = skrivUt + "\n" + listeSendInn[i][1] + ": " + element.value; 
+        if (element.validity.valid === false) {
+            feilMelding += "\n" + listeSendInn[i][2];
+            oversikt += 1;
+        }; 
     };
-    let start = "Du har sendt inn en bordbestilling. Bestillingen inneholdt:"
-    let avslutning = "\nVi gleder oss til å se deg!";
-    riktigTid();
-    alert(start + skrivUt + avslutning);
+    if (oversikt === 0) {
+        let start = "Du har sendt inn en bordbestilling. Bestillingen inneholdt:"
+        let avslutning = "\nVi gleder oss til å se deg!";
+        alert(start + skrivUt + avslutning);
+    } else {
+        let feilStart = "Du har fått denne feilmelding: ";
+        let feilAvslutt = "\nDu må rette opp i disse feilene før du sender bestillingen";
+        alert(feilStart + feilMelding + feilAvslutt);
+    };
+    
+    
 };
 
